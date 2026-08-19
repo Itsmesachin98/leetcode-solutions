0f07c4025bc7c644ac6a4e8dd7a56f34bbf12980
Sachin Kumar <sachinkumarskpog@gmail.com>
2026-08-19 11:52:40 +0530

    Solve Leetcode #203: Remove Linked List Elements

---
+++ linked_list/203/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int value, Node* address = nullptr) {
+        val = value;
+        next = address;
+    }
+};
+
+Node* convertArrToLinkedList(vector<int>& arr) {
+    if (arr.size() == 0) return nullptr;
+
+    Node* head = new Node(arr[0]);
+    Node* current = head;
+
+    for (int i = 1; i < arr.size(); i++) {
+        current->next = new Node(arr[i]);
+        current = current->next;
+    }
+
+    return head;
+}
+
+void print(Node* head) {
+    Node* current = head;
+
+    while (current != nullptr) {
+        cout << current->val << " ";
+        current = current->next;
+    } cout << endl;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 6, 3, 4, 5, 6};
+    int val = 6;
+
+    Node* head = convertArrToLinkedList(arr);
+    
+    if (!head) return 0;
+
+    while (head && head->val == val) {
+        head = head->next;
+    }
+
+    Node* result = head;
+    Node* block1 = new Node(-1);
+    Node* block2 = head;
+
+    while(block2) {
+        if (block2->val != val) {
+            block1->next = block2;
+            block1 = block2;
+        }
+
+        block2 = block2->next;
+    }
+
+    block1->next = nullptr;
+
+    print(result);
+}
