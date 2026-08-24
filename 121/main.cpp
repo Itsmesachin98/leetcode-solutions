15d2c3b3e3213e9b4712faf2ebae1271a0f6fe2a
Sachin Kumar <sachinkumarskpog@gmail.com>
2026-08-24 20:04:03 +0530

    Solve Leetcode #121: Best Time to Buy and Sell Stock

---
+++ 121/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int lowestPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < prices.size(); i++) {
+        maxProfit = max(maxProfit, (prices[i] - lowestPrice));
+        lowestPrice = min(lowestPrice, prices[i]);
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    vector<int> prices = {7,1,5,3,6,4};
+
+    int result = maxProfit(prices);
+
+    cout << result << endl;
+
+    return 0;
+}
